3e9c41656adb2408eb9b2cde4ea9606333311060
Chris Van Huffe <cvanhuffel@outlook.com>
2021-01-21 21:53:40 -0800

    Add, Get and Clear Contacts integration to back-end added

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -13,10 +13,17 @@ import {
 
 export default (state, action) => {
   switch(action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false
+      };
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload]
+        contacts: [...state.contacts, action.payload],
+        loading: false
       };
     case UPDATE_CONTACT:
       return {
@@ -24,14 +31,24 @@ export default (state, action) => {
         contacts: state.contacts.map(
           contact => contact.id === action.payload.id ? 
           action.payload : contact
-        )
+        ),
+        loading: false
       };
     case DELETE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.filter(
           contact => contact.id !== action.payload
-        )
+        ),
+        loading: false 
+      };
+    case CLEAR_CONTACTS:
+      return {
+        ...state,
+        contacts: null,
+        filtered: null,
+        error: null,
+        current: null
       };
     case SET_CURRENT:
       return {
@@ -57,6 +74,11 @@ export default (state, action) => {
       return {
         ...state,
         filtered: null
+      };
+    case CONTACT_ERROR:
+      return {
+        ...state,
+        error: action.payload
       };
     default:
       return state;
